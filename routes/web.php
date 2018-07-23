74efde8bcd6d79f4aecba0a23e5f5f28c47207df
vini123 <lichking_lin86@qq.com>
2018-07-23 11:24:36 +0000

    触发激活邮箱

--- routes/web.php
+++ routes/web.php
@@ -17,11 +17,13 @@ Auth::routes();
 
 Route::group(['middleware' => 'auth'], function() {
     Route::get('email_verify_notice', 'PageController@emailVerifyNotice')->name('email_verify_notice');
+    Route::get('email_verification/send', 'EmailVerificationController@send')->name('email_verification.send');
+    Route::get('email_verification/verify', 'EmailVerificationController@verify')->name('email_verification.verify');
 
     Route::group(['middleware' => 'email_verified'], function() {
         Route::get('/test', function() {
             return 'Your email is verified';
         });
     });
-    
+
 });
